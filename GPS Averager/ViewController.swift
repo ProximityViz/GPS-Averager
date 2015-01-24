6b2e8edc21822eb77d3e467f5adf608e2462e5d0
mollietaylor <mollie.taylor@gmail.com>
2015-01-24 14:11:31 -0500

    Begins storyboarding

--- GPS Averager/ViewController.swift
+++ GPS Averager/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var autoOrManual: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.startButton.layer.cornerRadius = 4
+        self.startButton.layer.borderWidth = 1
+        self.startButton.layer.borderColor = (UIColor (red:0.94, green:0.45, blue:0, alpha:1)).CGColor
+        
+        self.finishButton.layer.cornerRadius = 4
+        self.finishButton.layer.borderWidth = 1
+        self.finishButton.layer.borderColor = (UIColor (red:0.94, green:0.45, blue:0, alpha:1)).CGColor
+        
+    }
+    
+    @IBAction func changeMode(sender: UISegmentedControl) {
+        
+        var title = (self.autoOrManual.selectedSegmentIndex == 0) ? "Start" : "Add"
+        self.startButton.setTitle(title, forState: UIControlState.Normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
