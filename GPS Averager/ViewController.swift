dc170f16d08812b46d8cc9b3b4c99951c8210b4c
mollietaylor <mollie.taylor@gmail.com>
2015-01-31 15:19:24 -0500

    Gets manual mode working

--- GPS Averager/ViewController.swift
+++ GPS Averager/ViewController.swift
@@ -34,12 +34,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     var manager:CLLocationManager!
     var isRunning:Bool!
-    var mapCentered:Bool!
+//    var mapCentered:Bool!
     var mode:String!
+    
+    var LatLon: (latitude: Double, longitude: Double, latString: String, lonString:String)!
+    var userLocation:CLLocation!
     
     var latitudes = [Double]()
     var longitudes = [Double]()
     var altitudes = [Float]()
+    
+    var manualLats = [Double]()
+    var manualLons = [Double]()
+    var manualAlts = [Float]()
+    
+    let regularColor = UIColor.blackColor()
+    let boldColor = UIColor(red:0.99, green:0.13, blue:0.15, alpha:1)
     
     let defaults = NSUserDefaults.standardUserDefaults()
     
@@ -48,16 +58,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         // reset mapView
         mapView.removeAnnotations(mapView.annotations)
         
+        // reset labels
+        // TODO: should this reset to "current"?
+//        currentLabel.text = ""
+        currentLatLabel.text = ""
+        currentLonLabel.text = ""
+        currentAltLabel.text = ""
+        avgLatLabel.text = ""
+        avgLonLabel.text = ""
+        avgAltLabel.text = ""
+        avgPointsLabel.text = ""
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // TODO: These defaults will change once the user has the option of defaulting to manual mode
-        self.isRunning = false
-        self.mapCentered = false
-        self.mode = "Auto"
-        self.startButton.setTitle("Start", forState: UIControlState.Normal)
+        isRunning = false
+//        mapCentered = false
+        mode = "Auto"
+        startButton.setTitle("Start", forState: UIControlState.Normal)
         
         if coordFormat == nil {
             coordFormat = "Decimal degrees"
@@ -79,137 +100,78 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         mapView.removeAnnotations(mapView.annotations)
         
         // MARK: Aesthetics
+        startButton.layer.cornerRadius = 4
+        startButton.layer.borderWidth = 1
+        startButton.layer.borderColor = (UIColor (red:1.00, green:0.23, blue:0.19, alpha:1)).CGColor
         
-        self.startButton.layer.cornerRadius = 4
-        self.startButton.layer.borderWidth = 1
-        self.startButton.layer.borderColor = (UIColor (red:1.00, green:0.23, blue:0.19, alpha:1)).CGColor
-        
-        self.finishButton.layer.cornerRadius = 4
-        self.finishButton.layer.borderWidth = 1
-        self.finishButton.layer.borderColor = (UIColor (red:1.00, green:0.23, blue:0.19, alpha:1)).CGColor
+        finishButton.layer.cornerRadius = 4
+        finishButton.layer.borderWidth = 1
+        finishButton.layer.borderColor = (UIColor (red:1.00, green:0.23, blue:0.19, alpha:1)).CGColor
         
     }
     
     @IBAction func changeMode(sender: UISegmentedControl) {
         
-        var title = (self.autoOrManual.selectedSegmentIndex == 0) ? "Start" : "Add Point"
-        self.mode = (self.autoOrManual.selectedSegmentIndex == 0) ? "Auto" : "Manual"
-        self.currentLabel.text = (self.autoOrManual.selectedSegmentIndex == 0) ? "Most Recent" : "Current"
-        self.isRunning = false
-        self.startButton.setTitle(title, forState: UIControlState.Normal)
+        var title = (autoOrManual.selectedSegmentIndex == 0) ? "Start" : "Add Point"
+        mode = (autoOrManual.selectedSegmentIndex == 0) ? "Auto" : "Manual"
+        currentLabel.text = (autoOrManual.selectedSegmentIndex == 0) ? "Most Recent" : "Current"
+        isRunning = (autoOrManual.selectedSegmentIndex == 0) ? false : true
+        startButton.setTitle(title, forState: UIControlState.Normal)
         
     }
     
     @IBAction func startWasPressed(sender: UIButton) {
         
-        if self.mode == "Auto" && self.isRunning == false {
+        println(LatLon)
+        
+        if mode == "Auto" && isRunning == false {
             
             // mode is auto and has not begun yet
             // start averaging and change button to "Stop"
-            self.isRunning = true
-            self.startButton.setTitle("Stop", forState: UIControlState.Normal)
+            isRunning = true
+            startButton.setTitle("Stop", forState: UIControlState.Normal)
             
             // change label colors and/or column heading text to indicate "current" point is current
-            self.currentLabel.text = "Current"
+            currentLabel.text = "Current"
             
-        } else if self.mode == "Auto" && self.isRunning == true {
+        } else if mode == "Auto" && isRunning == true {
             
             // mode is auto and has been running
             // stop averaging and change button to "Start"
             // FIXME: decide if hitting the button again should restart or resume, and change text accordingly, and make sure it works properly
             
-            self.isRunning = false
-            self.startButton.setTitle("Start", forState: UIControlState.Normal)
+            isRunning = false
+            startButton.setTitle("Start", forState: UIControlState.Normal)
             // TODO: maybe change button color?
             
             // change label colors and/or column heading text to indicate "current" point is old
-            self.currentLabel.text = "Most Recent"
+            currentLabel.text = "Most Recent"
 
             
-        } else if self.mode == "Manual" {
+        } else if mode == "Manual" {
             
-            // FIXME: Manual mode not working yet
-            // have the currentlat, currentlon, and currentalt flash briefly in a different color when "Add Point" is pressed
-            // add point to array for averaging
-        }
-        
-    }
-    
-    @IBAction func finishWasPressed(sender: UIButton) {
-        
-        self.isRunning = false
-        
-        let avgCoords = Functions.averageCoordinates(latitudes, longitudes: longitudes)
-        
-        // average the altitudes
-        // FIXME: is there a built-in average function?
-        let avgAlt = Functions.averageOf(altitudes)
-        
-        // format date
-        var dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = .LongStyle
-        dateFormatter.timeStyle = .LongStyle
-        let formattedDate = dateFormatter.stringFromDate(NSDate())
-        
-        
-        savedAverages.append(["Latitude" : "\(avgCoords.avgLat)", "Longitude" : "\(avgCoords.avgLon)", "Altitude": "\(avgAlt) m", "Points" : "\(latitudes.count)", "Date" : "\(formattedDate)"])
-//        defaults.setObject(savedAverages, forKey: "savedAverages")
-        defaults.setValue(savedAverages, forKey: "savedAverages")
-        
-        // reset
-        latitudes = []
-        longitudes = []
-        altitudes = []
-        self.avgPointsLabel.text = ""
-        
-    
-    }
-    
-    
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        
-        var userLocation:CLLocation = locations[0] as CLLocation
-        
-        if !mapCentered {
+            // MARK: Change labels and map points for Manual mode
+            manualLats.append(LatLon.latitude)
+            manualLons.append(LatLon.longitude)
+            manualAlts.append(Float(userLocation.altitude))
             
-            // MARK: Zoom and center map
-            var mapLat:CLLocationDegrees = userLocation.coordinate.latitude
-            var mapLon:CLLocationDegrees = userLocation.coordinate.longitude
-            var span:MKCoordinateSpan = MKCoordinateSpanMake(0.005, 0.005)
-            var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(mapLat, mapLon)
-            var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            println(manualLats)
             
-            mapView.setRegion(region, animated: true)
-            mapCentered = true
+            currentLatLabel.textColor = boldColor
+            currentLonLabel.textColor = boldColor
+            currentAltLabel.textColor = boldColor
             
-        }
-        
-        
-        // MARK: Collect points
-        if self.isRunning == true {
-            
-            let LatLon = Functions.formatCoordinateString(lat: userLocation.coordinate.latitude, lon: userLocation.coordinate.longitude)
-            
-            currentLatLabel.text = LatLon.latString
-            currentLonLabel.text = LatLon.lonString
-            currentAltLabel.text = "\(userLocation.altitude) m"
-            
-            // MARK: Add point to arrays
-            latitudes.append(LatLon.latitude)
-            longitudes.append(LatLon.longitude)
-            altitudes.append(Float(userLocation.altitude))
-            
-            // MARK: Change "average" labels
-            let avgCoords = Functions.averageCoordinates(latitudes, longitudes: longitudes)
+            // change "average" labels
+            let avgCoords = Functions.averageCoordinates(manualLats, longitudes: manualLons)
             let latLonString = Functions.formatCoordinateString(lat: avgCoords.avgLat, lon: avgCoords.avgLon)
-            let avgAlt = Functions.averageOf(altitudes)
+            let avgAlt = Functions.averageOf(manualAlts)
             
             avgLatLabel.text = latLonString.latString
             avgLonLabel.text = latLonString.lonString
-            avgAltLabel.text = "\(avgAlt)"            
-            avgPointsLabel.text = "\(latitudes.count)"
+            avgAltLabel.text = "\(avgAlt)"
+            avgPointsLabel.text = "\(manualLats.count)"
             
-            // MARK: map points
+            // map points
             var mapLat:CLLocationDegrees = userLocation.coordinate.latitude
             var mapLon:CLLocationDegrees = userLocation.coordinate.longitude
             // TODO: change span to reflect where points are
@@ -221,6 +183,125 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             
             mapView.setRegion(region, animated: true)
             mapView.addAnnotation(annotation)
+            
+            
+            
+        }
+        
+    }
+    
+    @IBAction func finishWasPressed(sender: UIButton) {
+        // FIXME: add code for manual mode
+        
+        // format date
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .LongStyle
+        dateFormatter.timeStyle = .LongStyle
+        let formattedDate = dateFormatter.stringFromDate(NSDate())
+        
+        var avgCoords: (avgLat: Double, avgLon: Double)!
+        var avgAlt:Float = 0.0
+        var points:Int = 0
+        
+        if mode == "Auto" {
+        
+            isRunning = false
+            
+            avgCoords = Functions.averageCoordinates(latitudes, longitudes: longitudes)
+            
+            // average the altitudes
+            // FIXME: is there a built-in average function?
+            avgAlt = Functions.averageOf(altitudes)
+            points = latitudes.count
+        
+        } else {
+            
+            avgCoords = Functions.averageCoordinates(manualLats, longitudes: manualLons)
+            avgAlt = Functions.averageOf(manualAlts)
+            points = manualLats.count
+            
+        }
+        
+        savedAverages.append(["Latitude" : "\(avgCoords.avgLat)", "Longitude" : "\(avgCoords.avgLon)", "Altitude": "\(avgAlt) m", "Points" : "\(points)", "Date" : "\(formattedDate)"])
+        //        defaults.setObject(savedAverages, forKey: "savedAverages")
+        defaults.setValue(savedAverages, forKey: "savedAverages")
+        
+        // reset
+        latitudes = []
+        longitudes = []
+        altitudes = []
+        manualLats = []
+        manualLons = []
+        manualAlts = []
+        avgPointsLabel.text = ""
+        
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        userLocation = locations[0] as CLLocation
+        
+//        if !mapCentered {
+        
+        // MARK: Zoom and center map
+        var mapLat:CLLocationDegrees = userLocation.coordinate.latitude
+        var mapLon:CLLocationDegrees = userLocation.coordinate.longitude
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(0.005, 0.005)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(mapLat, mapLon)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        mapView.setRegion(region, animated: true)
+//            mapCentered = true
+        
+//        }
+        
+        
+        // MARK: Collect points
+        if isRunning == true {
+            
+            LatLon = Functions.formatCoordinateString(lat: userLocation.coordinate.latitude, lon: userLocation.coordinate.longitude)
+            
+            // MARK: Add point to arrays
+            latitudes.append(LatLon.latitude)
+            longitudes.append(LatLon.longitude)
+            altitudes.append(Float(userLocation.altitude))
+            
+            // change "current" labels
+            currentLatLabel.textColor = regularColor
+            currentLonLabel.textColor = regularColor
+            currentAltLabel.textColor = regularColor
+            currentLatLabel.text = LatLon.latString
+            currentLonLabel.text = LatLon.lonString
+            currentAltLabel.text = "\(userLocation.altitude) m"
+            
+            // MARK: Change labels and map points for Auto mode
+            if mode == "Auto" {
+                
+                // change "average" labels
+                let avgCoords = Functions.averageCoordinates(latitudes, longitudes: longitudes)
+                let latLonString = Functions.formatCoordinateString(lat: avgCoords.avgLat, lon: avgCoords.avgLon)
+                let avgAlt = Functions.averageOf(altitudes)
+                
+                avgLatLabel.text = latLonString.latString
+                avgLonLabel.text = latLonString.lonString
+                avgAltLabel.text = "\(avgAlt)"            
+                avgPointsLabel.text = "\(latitudes.count)"
+                
+                // map points
+                var mapLat:CLLocationDegrees = userLocation.coordinate.latitude
+                var mapLon:CLLocationDegrees = userLocation.coordinate.longitude
+                // TODO: change span to reflect where points are
+                var span:MKCoordinateSpan = MKCoordinateSpanMake(0.005, 0.005)
+                var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(mapLat, mapLon)
+                var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+                var annotation = MKPointAnnotation()
+                annotation.coordinate = location
+                
+                mapView.setRegion(region, animated: true)
+                mapView.addAnnotation(annotation)
+                
+            }
             
         }
         
